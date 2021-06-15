e1d378f0410982cde1b8b9acd50ff2adc0f643d5
Joao Lopes <jandrevl@gmail.com>
2021-06-15 20:14:28 +0100

    one more test string

--- src/com/ironhack/challenges1/SalvatoreChall20210615.java
+++ src/com/ironhack/challenges1/SalvatoreChall20210615.java
@@ -5,10 +5,11 @@ public class SalvatoreChall20210615 {
     public static void main(String[] args) {
         int[] test1 = {0}; //should return [0]
         int[] test2 = {1, 2, 0, 3, 0, 4, 0}; // should return [1, 2, 3, 4, 0, 0, 0]
-
+        int[] test3 = {1, 2, 3, 4, 5, 6}; //no change here
 
         System.out.println(Arrays.toString(zeroesEnd(test1)));
         System.out.println(Arrays.toString(zeroesEnd(test2)));
+        System.out.println(Arrays.toString(zeroesEnd(test3)));
     }
 
     public static int[] zeroesEnd (int[] input) {
